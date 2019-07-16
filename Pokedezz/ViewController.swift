5633ae973f7ea7dbbe087debf8d158aea259c8f1
Oscar Martin <martinm.oscar@gmail.com>
2019-07-16 15:09:22 +0200

    Initial Commit

---
+++ Pokedezz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokedezz
+//
+//  Created by Oscar Martín on 16/07/2019.
+//  Copyright © 2019 Oscar Martín. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
