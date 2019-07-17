c17a9cd9ef22a0c9c00456a61c27917063dcfff0
Oscar Martin <martinm.oscar@gmail.com>
2019-07-18 00:12:21 +0200

    awaiting

--- Pokedezz/ViewController.swift
+++ Pokedezz/ViewController.swift
@@ -7,14 +7,42 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
-
+    var pokemonDict: [Int: String] = [:]
+    
+    func downloadDataFromAPI(completion : @escaping ()->()){
+        
+        for index in 1...5 {
+            Alamofire.request("https://pokeapi.co/api/v2/pokemon/" + String(index)).responseJSON { response in
+                
+                let swiftyJsonVar = JSON(response.data!)
+                let pokemonName = swiftyJsonVar["name"].string
+                if let pokemonName = pokemonName {
+                    self.pokemonDict[index] = pokemonName
+                }
+                if(self.pokemonDict.count == 5){
+                    // When we finish to iterate
+                    completion()
+                }
+            }
+        }
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        downloadDataFromAPI(completion:
+            {
+                print(self.pokemonDict.count)
+        })
     }
-
-
+    
+    
 }
 
