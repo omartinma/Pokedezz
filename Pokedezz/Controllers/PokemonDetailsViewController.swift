919b43e9e7f6fb6c4bbe115e86f163ca1e3ca753
Oscar Martin <martinm.oscar@gmail.com>
2019-07-30 00:27:17 +0200

    Setting shadow and border

--- Pokedezz/Controllers/PokemonDetailsViewController.swift
+++ Pokedezz/Controllers/PokemonDetailsViewController.swift
@@ -11,6 +11,7 @@ import SDWebImage
 
 class PokemonDetailsViewController: UIViewController {
     
+    @IBOutlet weak var myLabel: UILabel!
     @IBOutlet weak var myStack: UIStackView!
     @IBAction func backAction(_ sender: Any) {
         dismiss(animated: true, completion: nil)
@@ -22,74 +23,80 @@ class PokemonDetailsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
         if let poke = pokemon{
+            // Background of the view according the pokemon color
             view.setGradientBackgroundColor(colorOne: (poke.colors[0]), colorTwo: (poke.colors[1]))
             pokemonLabelName.text = poke.name.capitalizingFirstLetter()
-            
             //Getting new better image
             let urlString = "https://pokeres.bastionbot.org/images/pokemon/" + String(poke.id) + ".png"
             pokemonImageView.sd_setImage(with: URL(string: urlString))
             
-            // Creating the view
-            let view1 = UIView()
-            view1.backgroundColor = .red
-            view1.layer.borderColor = UIColor.black.cgColor
-            view1.layer.borderWidth = 1
-            view1.layer.cornerRadius = 10
-            view1.clipsToBounds = true
             
+            // Creating the view for the pokemon type
+               // add the shadow to the base view
+            let pokemonTypeView = UIView()
+            pokemonTypeView.backgroundColor = UIColor.clear
+            pokemonTypeView.layer.shadowColor = UIColor.black.cgColor
+            pokemonTypeView.layer.shadowOffset = CGSize(width: 0, height: 1)
+            pokemonTypeView.layer.shadowOpacity = 0.1
+            pokemonTypeView.layer.shadowRadius = 2.0
             
-            // Creating the label inside the view
-            let yourLabel: PokemonTypeLabel = PokemonTypeLabel()
-            yourLabel.translatesAutoresizingMaskIntoConstraints = false
-            yourLabel.backgroundColor = .orange
-            yourLabel.textColor = .black
-            yourLabel.textAlignment = NSTextAlignment.center
-            yourLabel.text = "test"
-            view1.addSubview(yourLabel)
+            // add the border to subview, the one with the border
+            let pokemonTypeLabel: PokemonTypeLabel = PokemonTypeLabel()
+            pokemonTypeLabel.layer.masksToBounds = true
+            pokemonTypeLabel.translatesAutoresizingMaskIntoConstraints = false
+            pokemonTypeLabel.backgroundColor = poke.colors[0]
+            pokemonTypeLabel.textColor = .black
+            pokemonTypeLabel.textAlignment = NSTextAlignment.center
+            pokemonTypeLabel.text = "test"
+            pokemonTypeLabel.layer.cornerRadius = 10
+            pokemonTypeLabel.layer.borderColor = UIColor.clear.cgColor
+            pokemonTypeLabel.layer.borderWidth = 1.0
+            pokemonTypeView.addSubview(pokemonTypeLabel)
             
             // Constraints of the label
-            let widthLabelConstraint = NSLayoutConstraint(item: yourLabel, attribute: NSLayoutConstraint.Attribute.width, relatedBy: NSLayoutConstraint.Relation.equal, toItem: view1, attribute: NSLayoutConstraint.Attribute.width, multiplier: 1, constant: 0)
+            let widthLabelConstraint = NSLayoutConstraint(item: pokemonTypeLabel, attribute: NSLayoutConstraint.Attribute.width, relatedBy: NSLayoutConstraint.Relation.equal, toItem: pokemonTypeView, attribute: NSLayoutConstraint.Attribute.width, multiplier: 1, constant: 0)
 
-            let heigthLabelConstraint = NSLayoutConstraint(item: yourLabel, attribute: NSLayoutConstraint.Attribute.height, relatedBy: NSLayoutConstraint.Relation.equal, toItem: view1, attribute: NSLayoutConstraint.Attribute.height, multiplier: 1, constant: 0)
+            let heigthLabelConstraint = NSLayoutConstraint(item: pokemonTypeLabel, attribute: NSLayoutConstraint.Attribute.height, relatedBy: NSLayoutConstraint.Relation.equal, toItem: pokemonTypeView, attribute: NSLayoutConstraint.Attribute.height, multiplier: 1, constant: 0)
             NSLayoutConstraint.activate([widthLabelConstraint, heigthLabelConstraint])
   
             
-            myStack.addArrangedSubview(view1)
+            myStack.addArrangedSubview(pokemonTypeView)
             
-            
-            
-            
-            // Creating the view
-            let view2 = UIView()
-            view2.backgroundColor = .red
-            view2.layer.borderColor = UIColor.black.cgColor
-            view2.layer.borderWidth = 1
-            view2.layer.cornerRadius = 10
-            view2.clipsToBounds = true
-            
-            
-            // Creating the label inside the view
-            let yourLabel2: PokemonTypeLabel = PokemonTypeLabel()
-            yourLabel2.translatesAutoresizingMaskIntoConstraints = false
-            yourLabel2.backgroundColor = .orange
-            yourLabel2.textColor = .black
-            yourLabel2.textAlignment = NSTextAlignment.center
-            yourLabel2.text = "test longerrrr"
-            view2.addSubview(yourLabel2)
-            
-            // Constraints of the label
-            let widthLabelConstraint2 = NSLayoutConstraint(item: yourLabel2, attribute: NSLayoutConstraint.Attribute.width, relatedBy: NSLayoutConstraint.Relation.equal, toItem: view2, attribute: NSLayoutConstraint.Attribute.width, multiplier: 1, constant: 0)
-            
-            let heigthLabelConstraint2 = NSLayoutConstraint(item: yourLabel2, attribute: NSLayoutConstraint.Attribute.height, relatedBy: NSLayoutConstraint.Relation.equal, toItem: view2, attribute: NSLayoutConstraint.Attribute.height, multiplier: 1, constant: 0)
-            NSLayoutConstraint.activate([widthLabelConstraint2, heigthLabelConstraint2])
-            
-            
-            myStack.addArrangedSubview(view2)
-            
-            
-
-            
+//
+//
+//
+//            // Creating the view
+//            let view2 = UIView()
+//            view2.backgroundColor = .red
+//            view2.layer.borderColor = UIColor.black.cgColor
+//            view2.layer.borderWidth = 1
+//            view2.layer.cornerRadius = 10
+//            view2.clipsToBounds = true
+//
+//
+//            // Creating the label inside the view
+//            let yourLabel2: PokemonTypeLabel = PokemonTypeLabel()
+//            yourLabel2.translatesAutoresizingMaskIntoConstraints = false
+//            yourLabel2.backgroundColor = .orange
+//            yourLabel2.textColor = .black
+//            yourLabel2.textAlignment = NSTextAlignment.center
+//            yourLabel2.text = "test longerrrr"
+//            view2.addSubview(yourLabel2)
+//
+//            // Constraints of the label
+//            let widthLabelConstraint2 = NSLayoutConstraint(item: yourLabel2, attribute: NSLayoutConstraint.Attribute.width, relatedBy: NSLayoutConstraint.Relation.equal, toItem: view2, attribute: NSLayoutConstraint.Attribute.width, multiplier: 1, constant: 0)
+//
+//            let heigthLabelConstraint2 = NSLayoutConstraint(item: yourLabel2, attribute: NSLayoutConstraint.Attribute.height, relatedBy: NSLayoutConstraint.Relation.equal, toItem: view2, attribute: NSLayoutConstraint.Attribute.height, multiplier: 1, constant: 0)
+//            NSLayoutConstraint.activate([widthLabelConstraint2, heigthLabelConstraint2])
+//
+//
+//            myStack.addArrangedSubview(view2)
+//
+//
+//
+//
             
             
         
--- Pokedezz/Extension.swift
+++ Pokedezz/Extension.swift
@@ -30,6 +30,28 @@ extension UIView {
     }
 }
 
+extension PokemonTypeLabel {
+    func setGradientBackgroundColorLabel(colorOne: UIColor, colorTwo: UIColor) {
+        var gradientLayer = CAGradientLayer()
+        if let sublayers = layer.sublayers {
+            for sublayer in sublayers {
+                if let gLayer = sublayer as? CAGradientLayer {
+                    gradientLayer = gLayer
+                    break
+                }
+            }
+        }
+        
+        gradientLayer.frame = bounds
+        gradientLayer.colors = [colorOne.cgColor, colorTwo.cgColor]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.endPoint = CGPoint(x: 1.0, y: 1.0)
+        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.0)
+        
+        layer.insertSublayer(gradientLayer, at: 0)
+    }
+}
+
 extension String {
     func capitalizingFirstLetter() -> String {
         return prefix(1).uppercased() + self.lowercased().dropFirst()
